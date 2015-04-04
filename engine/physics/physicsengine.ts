b96b646c2df070dfccbc90723631207be3b12a46
Donald Jones <jones.donald@gmail.com>
2015-04-04 18:32:59 -0500

    [Timebox 2b] Finished gravity and friction.

--- engine/physics/physicsengine.ts
+++ engine/physics/physicsengine.ts
@@ -12,7 +12,7 @@ module Engine.Physics {
     export class PhysicsEngine {
         public keysDown: Keys;
 
-        constructor(public actors: Array<IMoves>, boundaries: Array<Boundary>) {
+        constructor(public actors: Array<IMoves>, boundaries: Array<Boundary>, public friction: number = 0.8, public gravity: number = 0.3) {
             this.keysDown = new Keys();
         }
 
@@ -21,18 +21,27 @@ module Engine.Physics {
 
             for (var i = 0; i < this.actors.length; i++) {
                 var actor = this.actors[i];
-
-                if (keys.left) {
-                    var newVelocity = actor.vx - 1;
-                    if (Math.abs(newVelocity) < actor.speed) {
-                        actor.vx = newVelocity;
+                if (actor.controllable) {
+                    if (keys.left) {
+                        var newVelocity = actor.vx - 1;
+                        if (Math.abs(newVelocity) < actor.speed) {
+                            actor.vx = newVelocity;
+                        }
                     }
-                }
 
-                if (keys.right) {
-                    var newVelocity = actor.vx + 1;
-                    if (Math.abs(newVelocity) < actor.speed) {
-                        actor.vx = newVelocity;
+                    if (keys.right) {
+                        var newVelocity = actor.vx + 1;
+                        if (Math.abs(newVelocity) < actor.speed) {
+                            actor.vx = newVelocity;
+                        }
+                    }
+
+                    if (keys.up) {
+                        if (!actor.jumping) {
+                            actor.vy -= actor.speed * 2;
+                            actor.jumping = true;
+                            console.log(actor.vy);
+                        }
                     }
                 }
             }
@@ -46,13 +55,19 @@ module Engine.Physics {
                 actor.dx = actor.x + actor.vx;
                 actor.dy = actor.y + actor.vy;
 
+                actor.y = actor.dy;
+
                 if (actor.dx >= 0 && actor.dx <= 640 - actor.width) {
                     actor.x += actor.vx;
+                    actor.vx *= this.friction;
                 }
 
-                if (actor.dy >= 0 && actor.dy <= 480 - actor.height) {
-                    actor.y += actor.vy;
+                if (actor.y >= 480 - actor.height) {
+                    actor.vy = 0;
+                    actor.jumping = false;
+                    actor.y = 480 - actor.height;
                 }
+                actor.vy += this.gravity;
             }
         }
 
